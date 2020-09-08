52d171ed7703dad43bd320fcdd57c5ebdc780968
Jonse178 <65801469+Jonse178@users.noreply.github.com>
2020-09-08 11:36:34 +0800

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -15,9 +15,6 @@ def main():
 def run():
    app.run(host="0.0.0.0", port=8080
    )
-   #app.run(host="0.0.0.0", port=3333, debug=False)
-   #app.run(host="0.0.0.0", port=8080, debug=True)
-   #@Werkzeug debugger
 
 def keep_alive():
     server = Thread(target=run)
